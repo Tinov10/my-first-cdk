6b0c3af5712d465a433397c4c3e7242df2681138
Martijn Versteeg <versteeg.ms@gmail.com>
2023-12-21 19:45:48 +0100

    codepipeline demo

--- lib/my-first-cdk-stack.ts
+++ lib/my-first-cdk-stack.ts
@@ -1,14 +1,35 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class MyFirstCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const bucketName = new cdk.CfnParameter(this, 'bucketName', {
+      type: 'String',
+      default: '',
+      description: 'S3 bucket NAME where Codepipeline will store lambda code',
+    });
+
+    const bucketKey = new cdk.CfnParameter(this, 'bucketKey', {
+      type: 'String',
+      default: '',
+      description:
+        'S3 bucket KEY which Codepipeline will use to store lambda code',
+    });
+
+    const bucket = s3.Bucket.fromBucketArn(
+      this,
+      'pipeline-bucket',
+      bucketName.valueAsString
+    );
+
     new lambda.Function(this, 'lambdaFunction', {
       functionName: 'first-cdk-lambda',
-      code: new lambda.AssetCode('src'),
+      // code: new lambda.AssetCode('src'),
+      code: lambda.Code.fromBucket(bucket, bucketKey.valueAsString)
       handler: 'index.handler',
       runtime: lambda.Runtime.NODEJS_16_X,
       memorySize: 128,
